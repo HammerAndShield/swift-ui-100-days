3abc3ff4fb6b836d8266fe9888f38cfc464dc194
austintownsend <austintownsend1234@gmail.com>
2025-05-16 08:44:25 -0700

    Initial Commit

---
+++ swift-ui-100-days/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-ui-100-days
+//
+//  Created by austin townsend on 5/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-ui-100-days/swift_ui_100_daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_100_daysApp.swift
+//  swift-ui-100-days
+//
+//  Created by austin townsend on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_100_daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
