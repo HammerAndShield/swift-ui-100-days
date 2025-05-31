2c6138c6f1cda362e7eb59978f7a16dbbe0a21df
austintownsend <austintownsend1234@gmail.com>
2025-05-31 16:34:13 -0700

    finish word scramble game

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by austin townsend on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
