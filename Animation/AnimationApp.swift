131fa9b3830ac4fd0092edfd63e21e9b52303e4d
austintownsend <austintownsend1234@gmail.com>
2025-06-01 22:29:00 -0700

    finish animation section

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by austin townsend on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
