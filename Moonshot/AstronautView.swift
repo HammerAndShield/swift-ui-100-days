e4964b67ed35b1892ae98997153a1d640b498c9e
austintownsend <austintownsend1234@gmail.com>
2025-06-07 21:35:18 -0700

    finish moonshot app

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    NavigationStack {
+        AstronautView(astronaut: astronauts["aldrin"]!)
+            .preferredColorScheme(.dark)
+    }
+}
+
+
---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by austin townsend on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
