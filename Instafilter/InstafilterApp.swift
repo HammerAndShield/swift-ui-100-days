7548676b1587ff441b5a329b10e282b1a6f30048
austintownsend <austintownsend1234@gmail.com>
2025-06-26 21:47:47 -0700

    add photos picker

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by austin townsend on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
